c66e562ed4f1b68f0c644d1e98ba2fa162f2f12c
Vitor Enes <vitorenesduarte@gmail.com>
2018-05-16 13:37:27 +0100

    finish app

--- app.py
+++ app.py
@@ -4,19 +4,24 @@ BASE_PORT = 8000
 
 id, children = util.parse_args()
 
-def send(msg):
-    print(str(id) + " received: " + str(msg))
+def forward(l):
+    # forward message to children
+    for child in children:
+        client = util.SimpleClient(port=BASE_PORT + child, retry=True)
+        client.send(l)
 
-print(BASE_PORT + id)
+def send(l):
+    print(str(id) + " received: " + str(l))
+    l.append(id)
+    forward(l)
 
 # create server
-server = SimpleServer(port=BASE_PORT + id, verbose=True)
+server = util.SimpleServer(port=BASE_PORT + id, verbose=True)
 server.register_function(send)
+print(server.list_functions())
 
-# create client
-for child in children:
-    client = SimpleClient(port=BASE_PORT + child, retry=True)
-    client.send("hi")
+# if 0, start sending
+if id == 0: forward([id])
 
 # server wait
 server.wait()
