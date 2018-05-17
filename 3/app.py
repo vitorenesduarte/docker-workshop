d83e5530423046dd0cbcd83fd5da3f5ac01cc330
Vitor Enes <vitorenesduarte@gmail.com>
2018-05-17 16:23:06 +0100

    move app

--- 3/app.py
+++ 3/app.py
@@ -1,8 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+import util
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+BASE_PORT = 8000
 
-app.run(host="0.0.0.0", debug=True)
+id, children = util.parse_args()
+
+def forward(l):
+    # forward message to children
+    for child in children:
+        client = util.SimpleClient(port=BASE_PORT + child, retry=True)
+        client.send(l)
+
+def send(l):
+    print(str(id) + " received: " + str(l))
+    if not id in l:
+        l.append(id)
+        forward(l)
+
+# create server
+server = util.SimpleServer(port=BASE_PORT + id, verbose=True)
+server.register_function(send)
+print(server.list_functions())
+
+# if 0, start sending
+if id == 0: forward([id])
+
+# server wait
+server.wait()
