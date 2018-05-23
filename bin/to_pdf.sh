d17dd7ec089034d07de9e0a3730b1d60e5ab451e
Vitor Enes <vitorenesduarte@gmail.com>
2018-05-23 11:16:05 +0100

    Generate pdfs with bigger font size

--- bin/to_pdf.sh
+++ bin/to_pdf.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DIR=$(dirname "${BASH_SOURCE[0]}")
+
 if [ $# -ne 1 ]; then 
     echo "usage: $0 FILE"
     exit
@@ -9,5 +11,23 @@ input=$1
 # remove extension
 name=$(echo ${input} | sed s/.md$//)
 output=${name}.pdf
+metadata_tmpl=${DIR}/metadata.yml
+metadata=${DIR}/.metadata.yml
 
-pandoc -V geometry:margin=1cm -r markdown -o ${output} ${input}
+# config
+margin=1cm
+fontsize=14pt
+
+# create metadata file
+# TODO: move margin config to metadata file
+cat ${metadata_tmpl} |
+    sed s/_FONTSIZE_/${fontsize}/ \
+    > ${metadata}
+
+# generate pdfs
+pandoc -V geometry:margin=${margin} \
+       -r markdown \
+       -o ${output} ${input} ${metadata}
+
+# remove metadata file
+rm ${metadata}
